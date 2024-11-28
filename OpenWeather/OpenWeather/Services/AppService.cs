4f0a9f13de6544d2d7c4b435470b995ac9c372e2
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2024-11-28 12:29:22 -0300

    feat: service e models atualizados

--- OpenWeather/OpenWeather/Services/AppService.cs
+++ OpenWeather/OpenWeather/Services/AppService.cs
@@ -2,7 +2,10 @@
 using OpenWeather.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -15,6 +18,7 @@ namespace OpenWeather.Services
         private HttpClient httpClient;
         private string ApiKey = "15d05aa6d398085bd0bc3088d06a1e49";
         private string BaseUrl = "https://api.openweathermap.org/data/2.5/weather";
+        public WeatherResponse WeatherResponse;
         private JsonSerializerOptions jsonSerializerOptions; 
         
         public AppService()
@@ -28,25 +32,57 @@ namespace OpenWeather.Services
         }
 
 
+      
+       ////PERSONALIZAR OS ICONS DE CLIMA
+
+        
         ///PROCURAR POR CIDADE
 
         public async Task<WeatherResponse> getWeatherbyCity(string cityName)
         {
             var url = $"{BaseUrl}?q={cityName}&appid={ApiKey}&units=metric&lang=pt";
-            return null;
-              
+        try
+        {
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();// tranforma o conteudo em string;
+            WeatherResponse = JsonSerializer.Deserialize<WeatherResponse>(content, jsonSerializerOptions);
         }
+    }
+    catch
+    {
+
+    }
+    return WeatherResponse;
+
+}
 
         //Procurar por coordenadas do gps
 
-        public async Task<WeatherResponse> getWeatherByGps(string lat, string lon)
+        public async Task<WeatherResponse> GetWeatherByGps(string lat, string lon)
         {
             var url = $"{BaseUrl}?lat={lat}&lon={lon}&appid={ApiKey}&units=metric&lang=pt";
-            return null;
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();// tranforma o conteudo em string;
+                    WeatherResponse = JsonSerializer.Deserialize<WeatherResponse>(content, jsonSerializerOptions);
+                }
+            }
+            catch
+            {
+
+            }
+            return WeatherResponse;
+
         }
 
 
-      
+
+
 
     }
 }
