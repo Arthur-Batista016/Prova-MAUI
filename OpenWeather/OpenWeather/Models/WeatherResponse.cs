c5dbef4a23235228a411687f26ac7c588a3225ff
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2024-11-28 12:07:33 -0300

    Service e Models atualizados

--- OpenWeather/OpenWeather/Models/WeatherResponse.cs
+++ OpenWeather/OpenWeather/Models/WeatherResponse.cs
@@ -9,12 +9,17 @@ namespace OpenWeather.Models
     internal class WeatherResponse
     {
         public Coord coord {  get; set; }
-        public Weather weather { get; set; }
+        public List<Weather> weather { get; set; }
+        public string Base {  get; set; } // modelar
         public Main main { get; set; }
-        public Visibility visibility { get; set; }
+        public int visibility { get; set; }
         public Wind wind { get; set; }
         public Rain rain { get; set; }
         public Clouds clouds { get; set; }
         public Sys sys { get; set; }
-    }
+        public int Timezone { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cod  { get; set; }
+            }
 }
